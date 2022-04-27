0ba519813505a9fe546813a339b594d06551edf1
Mirko Moschella <shary.mirko@gmail.com>
2022-04-27 15:39:01 +0200

    aggiungo il file php comics. richiamo i dati e creo i thumb

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,22 @@
 @extends('guest.template.base')
 @section('titolo', 'Meglio Marvel')
 @section('content')
-    
+    <div>
+        <div class="content">
+        </div>
+        <div class="fullwidth">
+            <div class="container">
+                <div class="actual-series">current series</div>
+                <div class="thumb-contain">
+                    @foreach ($comics as $item)
+                        <div class="thumbler">
+                            <img src="{{ $item['thumb'] }}" alt="{{ $item['title'] }}">
+                            <span>{{ $item['series'] }}</span>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="button">SING-UP NOW!</div>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $comics = config('comics');
+    $dati = [
+        'comics' => $comics
+    ];
+    return view('home', $dati);
 });
 
 Route::get('/comics', function () {
