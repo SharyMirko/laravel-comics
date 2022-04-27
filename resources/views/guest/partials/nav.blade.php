16f7f24e0aaafa15324491134e197822e309ffa3
Mirko Moschella <shary.mirko@gmail.com>
2022-04-27 17:16:44 +0200

    fixing the footer nav

--- resources/views/guest/partials/nav.blade.php
+++ resources/views/guest/partials/nav.blade.php
@@ -2,17 +2,17 @@
     <div class="container">
         <div class="nav">
             <img src="./images/dc-logo.png" alt="DC Logo">
-            <ul>
-                <li class=""><a href="#">characters</a></li>
-                <li class="active"><a href="#">comics</a></li>
-                <li class=""><a href="#">movies</a></li>
-                <li class=""><a href="#">tv</a></li>
-                <li class=""><a href="#">games</a></li>
-                <li class=""><a href="#">collectibles</a></li>
-                <li class=""><a href="#">videos</a></li>
-                <li class=""><a href="#">fans</a></li>
-                <li class=""><a href="#">news</a></li>
-                <li class=""><a href="#">shop</a></li>
+            <ul class="nav-ul">
+                <li class="nav-li"><a href="#">characters</a></li>
+                <li class="nav-li active"><a href="#">comics</a></li>
+                <li class="nav-li"><a href="#">tv</a></li>
+                <li class="nav-li"><a href="#">movies</a></li>
+                <li class="nav-li"><a href="#">games</a></li>
+                <li class="nav-li"><a href="#">collectibles</a></li>
+                <li class="nav-li"><a href="#">videos</a></li>
+                <li class="nav-li"><a href="#">fans</a></li>
+                <li class="nav-li"><a href="#">news</a></li>
+                <li class="nav-li"><a href="#">shop</a></li>
             </ul>
         </div>
     </div>
